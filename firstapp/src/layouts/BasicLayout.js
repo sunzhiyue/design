aa7dac90986741f3ee19ad975dcb10165bc51731
sunzhiyue <sunzhiue@outlook.com>
2017-12-02 20:45:21 +0800

    练习1

--- firstapp/src/layouts/BasicLayout.js
+++ firstapp/src/layouts/BasicLayout.js
@@ -1,76 +1,43 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-// import { connect } from 'dav';
-import styles from './BaisLayout.less';
+import { Layout, Icon } from 'antd';
+// import default from 'antd/lib/row';
 
-const { Header, Content, Footer, Sider } = Layout;
-const SubMenu = Menu.SubMenu;
+const { Header, Sider, Content } = Layout;
 
 class BasicLayout extends React.Component {
   state = {
     collapsed: false,
   };
-  onCollapse = (collapsed) => {
-    console.log(collapsed);
-    this.setState({ collapsed });
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
   }
   render() {
     return (
-      <Layout style={{ minHeight: '100vh' }}>
+      <Layout>
         <Sider
-          style={{ background: 'red' }}
+          trigger={null}
           collapsible
           collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
         >
-          <div className={styles.logo} style={{ background: 'red' }} />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="pie-chart" />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop" />
-              <span>Option 2</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="user" /><span>User</span></span>}
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={<span><Icon type="team" /><span>Team</span></span>}
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-              <Icon type="file" />
-              <span>File</span>
-            </Menu.Item>
-          </Menu>
+          <div className="logo" />
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-              Bill is a cat.
-            </div>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Content
           </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©2016 Created by Ant UED
-          </Footer>
         </Layout>
       </Layout>
     );
   }
 }
+
 export default BasicLayout;
